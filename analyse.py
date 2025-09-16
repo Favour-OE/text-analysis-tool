9ca09a5cce59d2cebb699f94868ba6694493ac1d
Favour Emmanuel <emmanuelojochenemi1111@gmail.com>
2025-09-16 15:57:45 +0100

    Converted workflows into functions

--- analyse.py
+++ analyse.py
@@ -1,11 +1,23 @@
-print("\nWelcome to the text analysis tool, i will mine and analyse a body of text, from a file you give me")
+#Welcome the User
+def welcomeUser():
+    print(
+        "\nWelcome to the text analysis tool, i will mine and analyse a body of text, from a file you give me"
+    )
+
 
 # Get Username
+def getUsername():
+    # Print message prompting user to input their name
+    usernameFromInput = input("\nTo begin, please enter your username: \n")
+    return usernameFromInput
 
-# Print message prompting user to input their name
+# Greet the user
+def greetUser(name, instruction):
+    print("Hello, " + name.upper())
 
-userName = input("\nTo begin, please enter your username: \n")
-#Greet the User
-print("Hello, " + userName)
+
+welcomeUser()
+username = getUsername()
+greetUser(username )
 
 
