1321ef9fe435a3c8d8eec194d0dbdc8f7df00926
Favour Emmanuel <emmanuelojochenemi1111@gmail.com>
2025-11-03 09:32:58 +0100

    Got Basic Stock Info

--- stockAnalyzer.py
+++ stockAnalyzer.py
@@ -1,5 +1,7 @@
 import yfinance as yf
 
+from datetime import datetime
+
 
 def extractBasicInfo(data):
     keystoExcract = [
@@ -21,10 +23,43 @@ def extractBasicInfo(data):
     return basicInfo
 
 
+def getPriceHistory(company):
+    historyDf = company.history(period="12mo")
+    prices = historyDf["Open"].tolist()
+    dates = historyDf.index.strftime("%Y-%m-%d").tolist()
+
+    return {"price": prices, "dates": dates}
+
+
+def getEarningsDates(company):
+    earningsDatesDF = company.earnings_dates
+    allDates = earningsDatesDF.index.strftime("%Y-%m-%d").tolist()
+    datesObjects = [datetime.strptime(date, "%Y-%m-%d") for date in allDates]
+    currentDate = datetime.now()
+    futureDates = [
+        date.strftime("%Y-%m-%d") for date in datesObjects if date > currentDate
+    ]
+    return futureDates
+
+
+def getCompanyNews(company):
+    newslist = company.news
+    allNewsArticles = []
+    for newsDict in newslist:
+        newsDictToAdd = {"title": newsDict['content']["title"], "URL": newsDict['content']["canonicalUrl"]['url']}
+        allNewsArticles.append(newsDictToAdd)
+    return allNewsArticles
+
+
 def getCompanyStockInfo(tickersSymbol):
+    # Get data from Yahoo Finance
     company = yf.Ticker(tickersSymbol)
+
+    # Get basic info from company
     basicInfo = extractBasicInfo(company.info)
-    print(basicInfo)
+    priceHistory = getPriceHistory(company)
+    futureearningsDates = getEarningsDates(company)
+    companyNews = getCompanyNews(company)
 
 
 getCompanyStockInfo("MSFT")
