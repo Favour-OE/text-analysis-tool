9c3095c77c22b536f62cda46d2cc227e59e98dc7
Favour Emmanuel <emmanuelojochenemi1111@gmail.com>
2025-11-04 11:24:05 +0100

    Setup article link request work flow

--- stockAnalyzer.py
+++ stockAnalyzer.py
@@ -1,5 +1,5 @@
 import yfinance as yf
-
+import requests
 from datetime import datetime
 
 
@@ -46,9 +46,27 @@ def getCompanyNews(company):
     newslist = company.news
     allNewsArticles = []
     for newsDict in newslist:
-        newsDictToAdd = {"title": newsDict['content']["title"], "URL": newsDict['content']["canonicalUrl"]['url']}
+        newsDictToAdd = {
+            "title": newsDict["content"]["title"],
+            "URL": newsDict["content"]["canonicalUrl"]["url"],
+        }
         allNewsArticles.append(newsDictToAdd)
+    # print( allNewsArticles)
     return allNewsArticles
+
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+    "AppleWebKit/537.36 (KHTML, like Gecko) "
+    "Chrome/120.0.0.0 Safari/537.36",
+    "Accept-Language": "en-US,en;q=0.9",
+}
+
+
+def extractCompanyNewsArticles(newsArticles):
+    url = newsArticles[0]["URL"]
+    page = requests.get(url, headers=headers)
+    print(page)
 
 
 def getCompanyStockInfo(tickersSymbol):
@@ -60,6 +78,8 @@ def getCompanyStockInfo(tickersSymbol):
     priceHistory = getPriceHistory(company)
     futureearningsDates = getEarningsDates(company)
     companyNews = getCompanyNews(company)
+    newsArticles = getCompanyNews(company)
+    extractCompanyNewsArticles(newsArticles)
 
 
 getCompanyStockInfo("MSFT")
